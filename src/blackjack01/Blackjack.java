1779478a7c1137cac4aec9ea6f28fc3b59f95753
Silver Piir <silverpiir@users.noreply.github.com>
2018-03-04 21:11:36 +0200

    start developing game loop

--- src/blackjack01/Blackjack.java
+++ src/blackjack01/Blackjack.java
@@ -20,5 +20,30 @@ public class Blackjack {
         Scanner userInput = new Scanner(System.in);
         //System.out.println(playingDeck);
 
+        while(playerMoney>0){
+            System.out.println("You have $" + playerMoney);
+            System.out.println("How much would you like to bet?");
+            double playerBet = userInput.nextDouble();
+
+            if(playerBet>playerMoney){
+                System.out.println("You don't have that much money!");
+                System.out.println("How much would you like to bet?");
+                playerBet = userInput.nextDouble();
+            }
+
+            // Card dealing
+            playerDeck.draw(playingDeck);
+            playerDeck.draw(playingDeck);
+            dealerDeck.draw(playingDeck);
+            dealerDeck.draw(playingDeck);
+
+            while(true){
+                System.out.println("Your hand: ");
+                System.out.print(playerDeck.toString());
+            }
+        }
+
+        System.out.println("You are out of money!");
+
     }
 }
